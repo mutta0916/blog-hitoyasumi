b2346f0b9b9999824c7a967d1df4fc6cfbe2cc5e
mutta0916 <mutta0916@gmail.com>
2022-12-17 23:31:56 +0900

    adjust footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,9 +12,9 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <header className={styles.title}>
           喫茶　ひとやすみ
-        </h1>
+        </header>
 
         <p className={styles.description}>
           Get started by editing{' '}
@@ -55,16 +55,7 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        <label className={styles.copyright}>©</label>2022 yurika
       </footer>
     </div>
   )
